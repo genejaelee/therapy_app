10cc2213fc1460bbfac006e5bf5c60e6901a3536
Gene Lee <genejaelee@gmail.com>
2014-03-06 17:26:48 -0300

    update application.rb

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(:default, Rails.env)
 module TherapyApp
   class Application < Rails::Application
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
