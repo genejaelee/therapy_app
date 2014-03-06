53f781c0bc19a5addcafc879ed312ead142171ba
Gene Lee <genejaelee@gmail.com>
2014-03-06 16:06:05 -0300

    hello

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TherapyApp::Application.config.secret_key_base = 'dd0497ec82c250a0a679e7e6861dee81c33feae64ca2c72c5aed57544891cf259d5af8cf49ac7e9911cd0297f3abfc9d581e2c14aa627f75bf5bd3ee7f12bb3c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TherapyApp::Application.config.session_store :cookie_store, key: '_therapy_app_session'
