8451daf71fc6e2f5f672103caec42d0385f3ee05
Gene Lee <genejaelee@gmail.com>
2014-03-07 03:35:56 -0300

    added line in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,9 @@
 TherapyApp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  
+  config.assets.compile = true
+  
+  config.assets.initialize_on_precompile = false
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -27,9 +31,6 @@ TherapyApp::Application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
-
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
