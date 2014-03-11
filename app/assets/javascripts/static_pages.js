17dd00938662288a1d5a97b4526aaa4a144466ca
Gene Lee <genejaelee@gmail.com>
2014-03-11 17:21:48 -0300

    accordion work, added jquery smoothness

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,5 +1,9 @@
 $( document ).ready(function() {
 	$('.cta-button2').click(function(){
-	    $('html, body').animate({scrollTop:0}, 1000, 'easeOutCubic');
+	  $('html, body').animate({scrollTop:0}, 1000, 'easeOutCubic');
 	});
+	
+  $(function() {
+	  $( "#accordion" ).accordion({header: "h3", collapsible: true, active: false, heightStyle: "content"});
+  });
 });
