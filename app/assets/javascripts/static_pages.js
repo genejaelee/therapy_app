edbd05189a554fe642b59f5502b944e55febd66a
Gene Lee <genejaelee@gmail.com>
2014-03-12 13:34:50 -0300

    fix jquery on reload

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,4 +1,4 @@
-$( document ).ready(function() {
+var ready = function() {
 	$('.cta-button2').click(function(){
 	  $('html, body').animate({scrollTop:0}, 1000, 'easeOutCubic');
 	});
@@ -6,4 +6,7 @@ $( document ).ready(function() {
   $(function() {
 	  $( "#accordion" ).accordion({header: "h3", collapsible: true, active: false, heightStyle: "content"});
   });
-});
+};
+
+$( document ).ready(ready);
+$( document ).on('page:load', ready);
