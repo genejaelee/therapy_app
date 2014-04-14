9bec02fb82b13e27ef1d81b9b80646fc0576d68f
Gene Lee <genejaelee@gmail.com>
2014-04-14 18:17:44 -0300

    adjust current time zone method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,6 +26,6 @@ class ApplicationController < ActionController::Base
   end
 
   def user_time_zone(&block)
-    Time.use_zone(current_user.time_zone, &block)
+    Time.use_zone(@_current_user.time_zone, &block)
   end
 end
