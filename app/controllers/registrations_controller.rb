49a73dadafe69e6f5e19f5848ed8e88674114391
Gene Lee <genejaelee@gmail.com>
2014-06-15 23:20:27 -0400

    fix therapist check if signed in

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,8 +2,14 @@ class RegistrationsController < Devise::RegistrationsController
   protected
   
   def after_sign_up_path_for(resource)
+    @therapist = current_therapist
+    puts "current therapist email is #{@therapist.email}"
+    @therapist.update_attribute(:time_zone, cookies["jstz_time_zone"])
+    return update_therapist_path
   end
   
   def edit
+    which_edit_page
+    @therapist = current_therapist
   end
 end
