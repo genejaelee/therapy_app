3ef99ed5054262a8365a0e3e2e750ff94b18a3fd
Gene Lee <genejaelee@gmail.com>
2014-06-17 13:10:49 -0400

    remove change descript type migration

--- db/migrate/20140617170339_change_description_type_in_clients.rb
+++
@@ -1,7 +0,0 @@
-class ChangeDescriptionTypeInClients < ActiveRecord::Migration
-  def change
-    change_column :clients, :encrypted_description, :string
-    change_column :clients, :encrypted_description_salt, :string
-    change_column :clients, :encrypted_description_iv, :string
-  end
-end
