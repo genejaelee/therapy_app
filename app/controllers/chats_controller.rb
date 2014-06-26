9519d51832af6fcf66439410cc0ce4e36841f5b8
Gene Lee <genejaelee@gmail.com>
2014-06-26 09:18:42 -0400

    generate chat on user create, require authentication before entering chat

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -24,7 +24,19 @@ class ChatsController < ApplicationController
     else
       redirect_to :controller => 'index', :action => 'index'
     end
-    
+  end
+  
+  def generate
+    chat = Chat.new
+    chat.owner = ChatUser.user(session)
+    chat.owner.update_attributes(:time_zone => cookies["jstz_time_zone"])
+    if chat.save
+      chat_tiny = Tiny::tiny(chat.id)
+      puts "chat id is #{chat.id}"
+        chat.channel = "message_channel_" + chat.id.to_s
+        chat.save
+      redirect_to :action => "view", :id => chat_tiny
+    end
   end
 
 end
