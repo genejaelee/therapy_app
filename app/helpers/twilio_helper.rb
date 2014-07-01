cb3827405b34609393370b05149292741423e903
Gene Lee <genejaelee@gmail.com>
2014-07-01 12:50:35 -0400

    test twilio with id numbers

--- app/helpers/twilio_helper.rb
+++ app/helpers/twilio_helper.rb
@@ -1,22 +1,24 @@
 module TwilioHelper 
   def client_name
     default_client = 'jenny'
-    @caller_email = current_user.email
+    @from = current_user.id
+    puts "from id is #{@from}"
     if current_user.role_type == "Therapist"
-      @email_to_call = Client.find_by(id: session[:chat_client_id]).user.email
+      @to_call = Client.find_by(id: session[:chat_client_id]).user.id
     elsif current_user.role_type == "Client"
-      @email_to_call = Therapist.find_by(id: session[:chat_therapist_id]).user.email
+      @to_call = Therapist.find_by(id: session[:chat_therapist_id]).user.id
     end
-    if @email_to_call.nil?
-      @email_to_call = default_client
+    if @to_call.nil?
+      @to_call = default_client
     end
-    return @email_to_call
+    puts "to id is #{@to_call}"
+    return @to_call
   end
   
   def twilio_token
     capability = Twilio::Util::Capability.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
     capability.allow_client_outgoing "AP1cee76b44225ccf0c74cffb3d1ef4078"
-    capability.allow_client_incoming :client_name
+    capability.allow_client_incoming 'therapistgmail.com'
     capability.generate
   end
 end
