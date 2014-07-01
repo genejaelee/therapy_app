f3e43c6a131546347b67a908af2cf4ba9be6af4d
Gene Lee <genejaelee@gmail.com>
2014-07-01 13:21:14 -0400

    remove token validation for hooks controller

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -1,4 +1,6 @@
 class HooksController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  
   def pusher_hooks
     webhook = Pusher::WebHook.new(request)
     if webhook.valid?
