f136dc585995c9f1bb82495d5260f664c2b23c34
Gene Lee <genejaelee@gmail.com>
2014-07-06 15:39:19 -0400

    rescue hooks errors

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -3,25 +3,29 @@ class HooksController < ApplicationController
   
   def pusher_hooks
     webhook = Pusher::WebHook.new(request)
-    if webhook.valid?
-      webhook.events.each do |event|
-        case event["name"]
-        when 'channel_occupied'
-          puts "Channel occupied: #{event["channel"]}"
-        when 'channel_vacated'
-          puts "Channel vacated: #{event["channel"]}"
-          Message.where(chat_id: session[:chat_id]).destroy_all
-          session[:chat_id] = nil
-        when 'member_added'
-          puts "Member added: #{event["channel"]}"
-          payload = { :user_id => event["user_id"], :nickname => ChatUser.find_by(id: event[:user_id]).nickname }
-          Pusher[event["channel"]].trigger('member_added', payload)
-          render :text => "MEMBER ADDED"
+    begin
+      if webhook.valid?
+        webhook.events.each do |event|
+          case event["name"]
+          when 'channel_occupied'
+            puts "Channel occupied: #{event["channel"]}"
+          when 'channel_vacated'
+            puts "Channel vacated: #{event["channel"]}"
+            Message.where(chat_id: session[:chat_id]).destroy_all
+            session[:chat_id] = nil
+          when 'member_added'
+            puts "Member added: #{event["channel"]}"
+            payload = { :user_id => event["user_id"], :nickname => ChatUser.find_by(id: event[:user_id]).nickname }
+            Pusher[event["channel"]].trigger('member_added', payload)
+            render :text => "MEMBER ADDED"
+          end
         end
+        render text: 'ok'
+      else
+        render text: 'invalid', status: 401
       end
-      render text: 'ok'
-    else
-      render text: 'invalid', status: 401
+    rescue
+      puts "hooks controller code error occurred"
     end
   end
 end
