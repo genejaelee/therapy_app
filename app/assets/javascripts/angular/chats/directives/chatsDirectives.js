43bcdeefd66e876d8a5c7ffa10d8973c403e826c
Gene Lee <genejaelee@gmail.com>
2014-07-23 14:43:46 -0400

    add xng focus on directive for focus input after sending

--- app/assets/javascripts/angular/chats/directives/chatsDirectives.js
+++ app/assets/javascripts/angular/chats/directives/chatsDirectives.js
@@ -46,3 +46,21 @@ chatsModule.directive('xngFocus', function(){
 			}, true);
 		};
 });
+
+chatsModule.directive('xngFocusOn', function($timeout, $rootScope) {
+  return {
+    restrict: 'A',
+    scope: {
+      focusValue: "=xngFocusOn"
+    },
+    link: function($scope, $element, attrs) {
+      $scope.$watch("focusValue", function(currentValue, previousValue) {
+        if (currentValue === true && !previousValue) {
+          $element[0].focus();
+        } else if (currentValue === false && previousValue) {
+          $element[0].blur();
+        }
+      })
+    }
+  }
+});
