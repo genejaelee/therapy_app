ca21e8e8077fe0e330e29cfaa49697ae7e96a26c
Gene Lee <genejaelee@gmail.com>
2014-07-31 17:29:58 -0700

    remove local time config line

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,6 @@ module TherapyApp
     end
     
     config.time_zone = 'Pacific Time (US & Canada)'
-    config.active_record.default_timezone = :local
     
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
