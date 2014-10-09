6d59580f8f1917b6de6028c0a045a72768fae624
Gene Lee <genejaelee@gmail.com>
2014-10-08 19:27:47 -0700

    simplify s3 dir

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -3,5 +3,5 @@ if Rails.env.development?
   Paperclip::Attachment.default_options[:path] = ":rails_root/public/system/:class/:id/:attachment/:basename_:style.:extension"
 elsif Rails.env.production?
   Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-  Paperclip::Attachment.default_options[:path] = '/:class/:id/:attachment/:basename_:style.:extension'
+  Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id/:basename_:style.:extension'
 end
